b0a27feead6519c8fcd8ba37f084e197038eef5c
HelenaLZ <helena.lz487@gmail.com>
2024-06-26 12:09:26 +0300

    Task3 26.06.24

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang = "en">
+<head>
+<meta charset = "UTF-8">
+<title>MySite</title>
+<link rel ="stylesheet" href="assets/css/main.css">
+</head>
+<body>
+<header>
+    <h2 class="SiteName">NIX MYSITE</h2>
+    <nav>
+        
+            <a href=# target="_blank">Lesson 1</a>
+            <a href=# target="_blank">Lesson 2</a>         
+    </nav>
+</header>
+    <section id="first">
+        <div class="title"><h1>Hello, <b>NIX Education</b></h1></div>
+    </section>
+<footer>Made by HelenaLZ&copy; 2024.</footer>  
+
+
+
+
+
+<!-- <script src="assets/js/jquery-3.6.0.min.js"></script>
+<script src="assets/js/main.js?1"></script> -->
+</body>
+</html>
+
+
+
